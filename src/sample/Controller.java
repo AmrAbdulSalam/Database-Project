5eedd6508389b91d73b58ba784178b8fb900bd29
Amr Abdulsalam <amrsalam@hotmail.com>
2020-07-18 01:35:05 +0300

    mainPage + logout

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,42 @@
 package sample;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Controller {
+    @FXML
+    private PasswordField passwordText;
+    @FXML
+    private TextField usernametext;
 
+    public void checkFields(ActionEvent actionEvent) throws IOException {
+        if (passwordText.getText().isEmpty() || usernametext.getText().isEmpty()){
+            JOptionPane.showMessageDialog(null , "You should sign in with your username and password");
+        }
+        else if (passwordText.getText().equals("123") && usernametext.getText().equals("amr")){
+
+            Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+            Scene tablescene = new Scene(root);
+            Stage window = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+            window.setScene(tablescene);
+            window.show();
+        }
+        else{
+            JOptionPane.showMessageDialog(null , "Undefined Username or Password");
+        }
+    }
+
+    public void aboutUs(MouseEvent mouseEvent) {
+        JOptionPane.showMessageDialog(null , "tsss");
+    }
 }
---
+++ src/sample/MainPage.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class MainPage {
+
+
+    public void checkFields(MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene tablescene = new Scene(root);
+        Stage window = (Stage)((Node) mouseEvent.getSource()).getScene().getWindow();
+        window.setScene(tablescene);
+        window.show();
+    }
+}
